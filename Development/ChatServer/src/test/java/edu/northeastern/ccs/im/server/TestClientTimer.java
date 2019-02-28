b3496febf0e08654e0577a773c27513984921dfc
sachinh <sachinh@ccs.neu.edu>
2019-02-28 18:13:11 -0500

    MSD201SP19-52 #comment Format code

--- Development/ChatServer/src/test/java/edu/northeastern/ccs/im/server/TestClientTimer.java
+++ Development/ChatServer/src/test/java/edu/northeastern/ccs/im/server/TestClientTimer.java
@@ -9,80 +9,82 @@ import java.util.GregorianCalendar;
 import org.junit.jupiter.api.Test;
 
 /**
- * TestClientTimer class contains the test suite tests the ClientTimer.java class under 
- * the im.server package. It tests all the different time instances after and before initialization and 
+ * TestClientTimer class contains the test suite tests the ClientTimer.java class under
+ * the im.server package. It tests all the different time instances after and before initialization and
  * activity
- *  @author Team 201 - rahul
  *
+ * @author Team 201 - rahul
  */
 public class TestClientTimer {
 
-	/**
-	 * Test for the constructor of Class ClientTimer
-	 */
-	@Test
-	public void testClientTimerConstructor() {
-		
-		ClientTimer ct  = new ClientTimer();
-		assertTrue(ClientTimer.class.isInstance(ct));			
-	}
+    /**
+     * Test for the constructor of Class ClientTimer
+     */
+    @Test
+    public void testClientTimerConstructor() {
 
-	/**
-	 * Test to check the time update after initialization function
-	 * @throws NoSuchFieldException
-	 * @throws SecurityException
-	 * @throws IllegalArgumentException
-	 * @throws IllegalAccessException
-	 */
-	@Test
-	public void testUpdateAfterInitialization() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
-		ClientTimer ct  = new ClientTimer();
-		Field privateStringField = ClientTimer.class.
-	            getDeclaredField("calendar");
-		privateStringField.setAccessible(true);
-		GregorianCalendar calendar = (GregorianCalendar) privateStringField.get(ct);
-		long timeAfterActivity = calendar.getTimeInMillis() ;
-		ct.updateAfterInitialization();
-		
-		assertTrue(Math.abs(calendar.getTimeInMillis() - timeAfterActivity) < 10); 
-		assertTrue(ClientTimer.class.isInstance(ct));
-		
-	}
-	
-	
-	/**
-	 * Test to check the time update after activity 
-	 * @throws NoSuchFieldException
-	 * @throws SecurityException
-	 * @throws IllegalArgumentException
-	 * @throws IllegalAccessException
-	 */
-	@Test
-	public void testupdateAfterActivity() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
-		ClientTimer ct  = new ClientTimer();
-		
-		Field privateStringField = ClientTimer.class.
-	            getDeclaredField("calendar");
+        ClientTimer ct = new ClientTimer();
+        assertTrue(ClientTimer.class.isInstance(ct));
+    }
 
-		privateStringField.setAccessible(true);
-		GregorianCalendar calendar = (GregorianCalendar) privateStringField.get(ct);
-		long timeAfterActivity = calendar.getTimeInMillis() + 17400000 ;
-		ct.updateAfterActivity();
-		assertTrue(Math.abs(calendar.getTimeInMillis() - timeAfterActivity) < 20);
-		assertTrue(ClientTimer.class.isInstance(ct)); 
-		
-	}
-	
-	/**
-	 * Test whether the calendar represents a time before the current time.
-	 */
-	@Test
-	public void testBehind() {
-		ClientTimer ct  = new ClientTimer();
-		ct.updateAfterInitialization();
-		assertTrue(ClientTimer.class.isInstance(ct));
-		assertEquals(ct.isBehind(), false);
-		
-		
-	}
+    /**
+     * Test to check the time update after initialization function
+     *
+     * @throws NoSuchFieldException
+     * @throws SecurityException
+     * @throws IllegalArgumentException
+     * @throws IllegalAccessException
+     */
+    @Test
+    public void testUpdateAfterInitialization() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+        ClientTimer ct = new ClientTimer();
+        Field privateStringField = ClientTimer.class.
+                getDeclaredField("calendar");
+        privateStringField.setAccessible(true);
+        GregorianCalendar calendar = (GregorianCalendar) privateStringField.get(ct);
+        long timeAfterActivity = calendar.getTimeInMillis();
+        ct.updateAfterInitialization();
+
+        assertTrue(Math.abs(calendar.getTimeInMillis() - timeAfterActivity) < 10);
+        assertTrue(ClientTimer.class.isInstance(ct));
+
+    }
+
+
+    /**
+     * Test to check the time update after activity
+     *
+     * @throws NoSuchFieldException
+     * @throws SecurityException
+     * @throws IllegalArgumentException
+     * @throws IllegalAccessException
+     */
+    @Test
+    public void testupdateAfterActivity() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+        ClientTimer ct = new ClientTimer();
+
+        Field privateStringField = ClientTimer.class.
+                getDeclaredField("calendar");
+
+        privateStringField.setAccessible(true);
+        GregorianCalendar calendar = (GregorianCalendar) privateStringField.get(ct);
+        long timeAfterActivity = calendar.getTimeInMillis() + 17400000;
+        ct.updateAfterActivity();
+        assertTrue(Math.abs(calendar.getTimeInMillis() - timeAfterActivity) < 20);
+        assertTrue(ClientTimer.class.isInstance(ct));
+
+    }
+
+    /**
+     * Test whether the calendar represents a time before the current time.
+     */
+    @Test
+    public void testBehind() {
+        ClientTimer ct = new ClientTimer();
+        ct.updateAfterInitialization();
+        assertTrue(ClientTimer.class.isInstance(ct));
+        assertEquals(ct.isBehind(), false);
+
+
+    }
 }

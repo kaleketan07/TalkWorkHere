97f3fc3ff73f1ef3bcb5df9162a038bd4d75ec08
Kunal Patil <patil.ku@husky.neu.edu>
2019-02-28 06:44:27 -0500

    MSD201SP19-25 #comment Added basic user services. Tests to be performed. There are some more design decisions to be made that will help in writing more functions

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/services/UserDao.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/services/UserDao.java
@@ -1,5 +1,6 @@
 package edu.northeastern.ccs.im.services;
 
+import java.sql.SQLException;
 import java.util.Set;
 
 import edu.northeastern.ccs.im.models.User;
@@ -13,26 +14,31 @@ public interface UserDao {
 	
 	
 	//Get user by userId integer value
-	User getUser(int userId);
+	User getUser(int userId) throws SQLException;
 	
 	//Get all the users for the system
-    Set<User> getAllUsers();
+    Set<User> getAllUsers() throws SQLException;
     
     //Get a specific user based on username and password
     User getUserByUserNameAndPassword(String username, String password);
     
     //Get specific user by username
-    User getUserByUserName(String username);
+    User getUserByUserName(String username) throws SQLException;
     
     //Create a User entry in database
-    boolean createUser(User u);
+    boolean createUser(User u) throws SQLException;
     
     // insert a User into database
-    boolean insertUser(User u);
+    /*
+        Kunal Note: Commenting insertUser because there doesn't seem to be
+                    a difference between createUser and insertUser from a
+                    DBMS point of view
+     */
+    //boolean insertUser(User u);
     
     // update a User details in database
     boolean updateUser(User u);
     
     // delete a User from database
-    boolean deleteUser(User u);
+    boolean deleteUser(User u) throws SQLException;
 }

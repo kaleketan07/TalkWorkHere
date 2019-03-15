81b5231fb82dfdd4b172890365dd7913edfa25d9
sachinh <sachinh@ccs.neu.edu>
2019-03-15 05:34:28 -0400

    MSD201SP19-88 #comment change jdbc connectivity

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/db/DBConnection.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/db/DBConnection.java
@@ -25,7 +25,11 @@ public class DBConnection {
         String driver = properties.getProperty("jdbc.driver");
         String username = properties.getProperty("jdbc.username");
         String password = properties.getProperty("jdbc.password");
-        Class.forName(driver);
+        try {
+            Class.forName(driver).newInstance();
+        } catch(Exception e) {
+            System.out.println(e);
+        }
         connection = DriverManager.getConnection(url, username, password);
     }
 

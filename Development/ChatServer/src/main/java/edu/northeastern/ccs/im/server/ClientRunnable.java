757e3328120353c39bb792d95b63f0abe9550c26
rahulbhat31 <rahulbhat1203@yahoo.in>
2019-04-03 17:03:08 -0400

    MSD201SP19-138 #comment handled leave group in clientRunnable

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/server/ClientRunnable.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/server/ClientRunnable.java
@@ -1257,28 +1257,28 @@ public class ClientRunnable implements Runnable {
      * @param msg the msg object of type Remove Group from Group
      * @throws SQLException the SQL exception
      */
-//    private void handleLeaveGroupMessage(Message msg) throws SQLException {
-//        User currentUser = userService.getUserByUserName(msg.getName());
-//        Group guestGroup = groupService.getGroup(msg.getTextOrPassword());
-//        try {
-//	        if(guestGroup == null) {
-//	        	this.enqueuePrattleResponseMessage("The group you are trying to leave does not exist");
-//	        }
-//	        else if (groupService.isModerator(guestGroup.getGroupName(), currentUser.getUserName())) {
-//	        	this.enqueuePrattleResponseMessage("As you are the moderator of the group you cannot leave the group "
-//	        			+ "Please transfer the ownership to someone else and then leave");
-//	        }
-//	        else if (groupService.checkMembershipInGroup(guestGroup.getGroupName(), currentUser.getUserName())){
-//	        	groupService.removeUserFromGroup(guestGroup.getGroupName(), currentUser.getUserName());
-//	        }
-//	        else {
-//	        	this.enqueuePrattleResponseMessage("You are not a member of the group you are trying to leave");
-//	        }
-//        }
-//        catch (SQLException e) {
-//        	this.enqueuePrattleResponseMessage(CHECK_SYNTAX_ERROR_MESSAGE);
-//		}
-//    }
+    private void handleLeaveGroupMessage(Message msg) throws SQLException {
+        User currentUser = userService.getUserByUserName(msg.getName());
+        Group guestGroup = groupService.getGroup(msg.getTextOrPassword());
+        try {
+	        if(guestGroup == null) {
+	        	this.enqueuePrattleResponseMessage("The group you are trying to leave does not exist");
+	        }
+	        else if (groupService.isModerator(guestGroup.getGroupName(), currentUser.getUserName())) {
+	        	this.enqueuePrattleResponseMessage("As you are the moderator of the group you cannot leave the group "
+	        			+ "Please transfer the ownership to someone else and then leave");
+	        }
+	        else if (groupService.checkMembershipInGroup(guestGroup.getGroupName(), currentUser.getUserName())){
+	        	groupService.removeUserFromGroup(guestGroup.getGroupName(), currentUser.getUserName());
+	        }
+	        else {
+	        	this.enqueuePrattleResponseMessage("You are not a member of the group you are trying to leave");
+	        }
+        }
+        catch (SQLException e) {
+        	this.enqueuePrattleResponseMessage(CHECK_SYNTAX_ERROR_MESSAGE);
+		}
+    }
 
     /**
      * This function retrieves all the messages that a user has received and formats a giant string to be sent to the
@@ -1402,9 +1402,9 @@ public class ClientRunnable implements Runnable {
         } else if (msg.isRemoveGroupFromGroupMessage()) {
             handleRemoveGroupFromGroupMessage(msg);
             return true;
-//        } else if (msg.isLeaveGroupMessage()) {
-//            handleLeaveGroupMessage(msg);
-//            return true;
+        } else if (msg.isLeaveGroupMessage()) {
+            handleLeaveGroupMessage(msg);
+            return true;
         }
         return false;
     }

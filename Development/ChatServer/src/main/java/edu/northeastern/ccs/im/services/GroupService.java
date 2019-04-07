e72d9737f1e2ba92aec6c75c318f05663f239623
kaleketan07 <kaleketan07@ccs.neu.edu>
2019-04-06 22:27:57 -0400

    MSD201SP19-149 #comment remove unused import code smell

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/services/GroupService.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/services/GroupService.java
@@ -8,8 +8,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-
-import edu.northeastern.ccs.im.ChatLogger;
 import edu.northeastern.ccs.im.db.DBConnection;
 import edu.northeastern.ccs.im.db.DBUtils;
 import edu.northeastern.ccs.im.db.IDBConnection;
